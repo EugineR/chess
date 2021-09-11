2f2ede1e3c269044bc64959baa7e12ee7acdad9a
Eugene Reshentyak <euginer96@gmail.com>
2021-09-11 13:07:19 +0200

    [feature] Gameboard component.
    - Implemented initial styles for board.
    - Updated angular.json with shortcuts.
    - Defined enum for figures.
    - Updated prettier config.

--- ng-chess/src/app/components/game-board/game-board.component.ts
+++ ng-chess/src/app/components/game-board/game-board.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component
+} from '@angular/core';
 
 @Component({
-  selector: 'app-game-board',
-  templateUrl: './game-board.component.html',
-  styleUrls: ['./game-board.component.scss']
+    selector: 'app-game-board',
+    templateUrl: './game-board.component.html',
+    styleUrls: ['./game-board.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GameBoardComponent implements OnInit {
+export class GameBoardComponent {
+    squares = new Array(64).fill(1);
 
-  constructor() { }
+    constructor(private readonly _changeDetectorRef: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
-  }
+    private static _isBlackSquare(i: number) {
+        return ((i % 8) + Math.floor(i / 8)) % 2;
+    }
 
+    getSquareClass(i: number) {
+        return {
+            [`${
+                GameBoardComponent._isBlackSquare(i) ? 'black' : 'white'
+            }-square`]: true
+        };
+    }
 }
---
+++ ng-chess/src/app/constants/figure.enum.ts
@@ -0,0 +1,14 @@
+export enum Figure {
+    WhiteKing = '&#9812;',
+    WhiteQueen = '&#9813;',
+    WhiteRook = '&#9814;',
+    WhiteBishop = '&#9815;',
+    WhiteKnight = '&#9816;',
+    WhitePawn = '&#9817;',
+    BlackKing = '&#9818;',
+    BlackQueen = '&#9819;',
+    BlackRook = '&#9820;',
+    BlackBishop = '&#9821;',
+    BlackKnight = '&#9822;',
+    BlackPawn = '&#9823;',
+}
